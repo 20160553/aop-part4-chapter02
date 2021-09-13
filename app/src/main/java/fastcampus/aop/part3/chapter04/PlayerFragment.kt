f176d49894e175c5b7c451e11a1553d77f6e65bc
20160553 <53904156+20160553@users.noreply.github.com>
2021-09-13 18:48:08 +0900

    Initial commit

---
+++ app/src/main/java/fastcampus/aop/part3/chapter04/PlayerFragment.kt
@@ -0,0 +1,19 @@
+package fastcampus.aop.part3.chapter04
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class PlayerFragment: Fragment(R.layout.fragment_player) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    companion object {
+        fun newInstance(): PlayerFragment {
+            return PlayerFragment()
+        }
+    }
+}
